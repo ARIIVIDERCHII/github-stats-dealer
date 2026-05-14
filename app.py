598b071e34a8c912a97f69077f70317847e15463
Daniil Agafonov <zverzerr@gmail.com>
2026-05-14 11:50:15 +0900

    fix: disable caching to allow animation restart

--- app.py
+++ app.py
@@ -79,7 +79,6 @@ def get_github_data():
 def github_stats_svg():
     top_langs, total_repos, total_commits, total_stars = get_github_data()
     
-    
     template_data = {
         "lang_1_name": top_langs[0][0], "lang_1_percent": top_langs[0][1],
         "lang_2_name": top_langs[1][0], "lang_2_percent": top_langs[1][1],
@@ -93,7 +92,11 @@ def github_stats_svg():
     
     response = make_response(svg_content)
     response.content_type = 'image/svg+xml'
-    response.headers['Cache-Control'] = 'public, max-age=14400'
+    
+    # СБРОС КЭША: Эти заголовки заставят GitHub каждый раз запрашивать новую анимацию
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
     
     return response
 
